e9a856e15f68a17c701aab61a26347911b96bb74
aletheia <aletheiak93@gmail.com>
2022-02-15 15:04:07 -0800

    db relationship between user and questions

--- app/models/questions.py
+++ app/models/questions.py
@@ -5,8 +5,10 @@ class Questions(db.Model):
     __tablename__ = 'questions'
 
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String, nullable=False, unique=True)
-    details = db.Column(db.String)
+    question = db.Column(db.String(500), nullable=False, unique=True)
+    details = db.Column(db.String(2000))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship("User", back_populates='questions')
 
 
