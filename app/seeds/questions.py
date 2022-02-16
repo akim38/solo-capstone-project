e50a53adcac5414c17c730387ec360c1008a28bf
aletheia <aletheiak93@gmail.com>
2022-02-15 19:58:43 -0800

    deleted extra s in Question, seeded question data

--- app/seeds/questions.py
+++ app/seeds/questions.py
@@ -1,14 +1,14 @@
-from app.models import db, Questions
+from app.models import db, Question
 
 
 def seed_questions():
 
-    question_01 = Questions(question='Why are plants green?', details='I noticed that most plants are green and would like to know more!', user_id=1)
-    question_02 = Questions(question='What are ways to bond with my cat?', details='My cat seems to hate me but I just want to be closer to my cat!', user_id=1)
-    question_03 = Questions(question='What is the best tasting sandwich?', user_id=1)
-    question_04 = Questions(question='How much is a florida ounce?', details='I see it on bottles all the time! What does a Florida ounce mean?', user_id=2)
-    question_05 = Questions(question='Where do we come from?', user_id=2)
-    question_06 = Questions(question='How do I become rich?', user_id=3)
+    question_01 = Question(question='Why are plants green?', details='I noticed that most plants are green and would like to know more!', user_id=1)
+    question_02 = Question(question='What are ways to bond with my cat?', details='My cat seems to hate me but I just want to be closer to my cat!', user_id=1)
+    question_03 = Question(question='What is the best tasting sandwich?', user_id=1)
+    question_04 = Question(question='How much is a florida ounce?', details='I see it on bottles all the time! What does a Florida ounce mean?', user_id=2)
+    question_05 = Question(question='Where do we come from?', user_id=2)
+    question_06 = Question(question='How do I become rich?', user_id=3)
 
     db.session.add(question_01)
     db.session.add(question_02)
