a74d938fcc63708514a9eaf6a0f25961f66f171d
aletheia <aletheiak93@gmail.com>
2022-02-16 09:34:36 -0800

    created post route for questions

--- app/api/question_routes.py
+++ app/api/question_routes.py
@@ -1,6 +1,9 @@
-from crypt import methods
-from flask import Blueprint
-from app.models import Question
+from flask import Blueprint, request
+from app.api.auth_routes import validation_errors_to_error_messages
+from app.models import db, Question
+from app.forms import QuestionForm
+from flask_login import current_user, login_required
+
 
 question_routes = Blueprint('questions', __name__)
 
@@ -21,10 +24,27 @@ def question(id):
 
 # post question
 @question_routes.route('/', methods=['POST'])
+@login_required
 def post_question():
-    
+    form = QuestionForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_question = Question(
+            question=form.data['question'],
+            details=form.data['details'],
+            user_id=current_user.id
+        )
+        db.session.add(new_question)
+        db.session.commit()
+        return new_question.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
 
 # edit question
-
+@question_routes.route('/<int:id>/', methods=['PUT'])
+@login_required
 
 # delete question
+@question_routes.route('/<int:id>/', methods=['DELETE'])
+@login_required
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,3 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
-from .question_form 
+from .question_form import QuestionForm
