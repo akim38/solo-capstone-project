adb38f0a1845aadc87b1ab3d97407a343d669318
aletheia <aletheiak93@gmail.com>
2022-02-16 09:57:32 -0800

    edit and delete api routes for questions

--- app/api/question_routes.py
+++ app/api/question_routes.py
@@ -17,6 +17,7 @@ def questions():
 
 # get question based on id
 @question_routes.route('/<int:id>/')
+@login_required
 def question(id):
     question = Question.query.get(id)
     return question.to_dict()
@@ -40,11 +41,25 @@ def post_question():
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
-
 # edit question
 @question_routes.route('/<int:id>/', methods=['PUT'])
 @login_required
+def edit_question(id):
+    question = Question.query.get(id)
+    form = QuestionForm()
+    question.question = form.data['question']
+    question.details = form.data['details']
+
+    db.session.add(question)
+    db.session.commit()
+    return question.to_dict()
+
 
 # delete question
 @question_routes.route('/<int:id>/', methods=['DELETE'])
 @login_required
+def delete_question(id):
+    question = Question.query.get(id)
+    db.session.delete(question)
+    db.session.commit()
+    return {"message": "Question deleted."}
