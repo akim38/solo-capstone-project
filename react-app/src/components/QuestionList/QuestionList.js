ab94badfb9b5f3f21d0c2a47206c6f5362b38355
aletheia <aletheiak93@gmail.com>
2022-02-16 15:27:22 -0800

    question list shows questions

--- react-app/src/components/QuestionList/QuestionList.js
+++ react-app/src/components/QuestionList/QuestionList.js
@@ -1,22 +1,28 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { getQuestions } from '../../store/questions';
 
 function QuestionList() {
     const dispatch = useDispatch();
-    const questions = useSelector(state => state.questions);
+    const questions = useSelector(state => state.questions.byId);
     console.log(questions)
+    const questionList = Object.values(questions).reverse();
+    console.log(questionList)
 
     useEffect(() => {
         dispatch(getQuestions());
     }, [dispatch])
 
 
-
     return (
         <>
             <div>
-                This is where questions will go
+                {questionList.map(question => (
+                    <NavLink key={question.question} to={`/questions/${question.id}`}>
+                        <h3>{question.question}</h3>
+                    </NavLink>
+                ))}
             </div>
         </>
     )
