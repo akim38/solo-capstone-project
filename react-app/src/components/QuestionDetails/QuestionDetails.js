90f06256ed05492aeea988a46278cc8e32e3af00
aletheia <aletheiak93@gmail.com>
2022-02-17 23:35:45 -0800

    added delete question button

--- react-app/src/components/QuestionDetails/QuestionDetails.js
+++ react-app/src/components/QuestionDetails/QuestionDetails.js
@@ -1,27 +1,34 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom"
-import { getSingleQuestion } from "../../store/questions";
+import { useHistory, useParams } from "react-router-dom"
+import { getSingleQuestion, removeQuestion } from "../../store/questions";
 
 
 const QuestionDetails = () => {
     const { questionId } = useParams();
+    const history = useHistory();
 
     const dispatch = useDispatch();
     const question = useSelector(state => state.questions.byId[questionId])
 
     useEffect(() => {
         dispatch(getSingleQuestion(questionId));
-    }, [dispatch])
+    }, [dispatch, questionId])
 
-    console.log(question)
+    const deleteQuestion = async (e) => {
+        e.preventDefault()
 
+        await dispatch(removeQuestion(questionId))
+            .then((res) => history.push('/questions/'))
+    }
 
     return (
         <div>
             <h2>{question?.question}</h2>
             <p>Asked by {question?.username}</p>
             <p>{question?.details}</p>
+            {/* <button className="edit-question-button" onClick={editQuestion}>Edit</button> */}
+            <button type="submit" className="delete-question-button" onClick={deleteQuestion}>Delete</button>
         </div>
 
     )
