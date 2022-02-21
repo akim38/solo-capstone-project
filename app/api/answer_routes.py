024027db3ccf667bccff3340adfd0c4e087d3e03
aletheia <aletheiak93@gmail.com>
2022-02-21 10:26:14 -0800

    completed all api routes for answers

--- app/api/answer_routes.py
+++ app/api/answer_routes.py
@@ -2,9 +2,11 @@ from flask import Blueprint, request
 from app.api.auth_routes import validation_errors_to_error_messages
 from app.models import db, Question, User, Answer
 from flask_login import current_user, login_required
+from app.forms import AnswerForm
 
 
 answer_routes = Blueprint('answers', __name__)
+
 
 #get all answers
 @answer_routes.route('/')
@@ -26,16 +28,47 @@ def answer(id):
 
     return {'answers': [answer_info]}
 
+
 #post answer on question
 @answer_routes.route('/', methods=['POST'])
 @login_required
-def post_answer():
-    
+def post_answer(question_id):
+    form = AnswerForm()
+    form['csrf_token'.data] = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_answer = Answer(
+            answer=form.data['answer'],
+            user_id=current_user.id,
+            question_id=question_id
+        )
+        db.session.add(new_answer)
+        db.session.commit()
+        return new_answer.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
 #edit answer
+@answer_routes.route('/<int:id>/', methods=['PUT'])
+@login_required
+def edit_answer(id):
+    answer = Answer.query.get(id)
+    form = AnswerForm()
+    if form.validate_on_submit():
+        answer.answer = form.data['answer']
 
+        db.session.commit()
+        return answer.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 #delete answer
+@answer_routes.route('/<int:id>/', methods=['DELETE'])
+@login_required
+def delete_answer(id):
+    answer = Answer.query.get(id)
+    db.session.delete(answer)
+    db.session.commit()
+    return {'message': 'Answer deleted.'}
+
 
 
 
--- app/forms/answer_form.py
+++ app/forms/answer_form.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, ValidationError, Length
-from app.models import Answer
+from wtforms.validators import DataRequired, Length
 
 
 class AnswerForm(FlaskForm):
