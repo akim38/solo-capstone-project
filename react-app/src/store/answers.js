76fb928a9a27dadff75ba3d37674d2e71110e082
aletheia <aletheiak93@gmail.com>
2022-02-21 11:11:45 -0800

    created action creators for answer store

--- react-app/src/store/answers.js
+++ react-app/src/store/answers.js
@@ -1,10 +1,24 @@
 //action types
 const LOAD_ANSWERS = 'answer/LOAD';
 const ADD_ANSWER = 'answer/ADD';
-const DELETE_ANSWER = 'answer/DELETE'; 
+const DELETE_ANSWER = 'answer/DELETE';
 
 
 //action creators
+const loadAnswers = (answers) => ({
+    type: LOAD_ANSWERS,
+    answers
+});
+
+const addAnswer = (answer) => ({
+    type: ADD_ANSWER,
+    answer
+});
+
+const deleteAnswer = (answerId) => ({
+    type: DELETE_ANSWER,
+    answerId
+})
 
 
 //thunks
