9e8b67d6de4779dd34231390d8e7c122d3205240
aletheia <aletheiak93@gmail.com>
2022-02-21 09:18:38 -0800

    added get routes and answer form

--- app/api/answer_routes.py
+++ app/api/answer_routes.py
@@ -6,14 +6,31 @@ from flask_login import current_user, login_required
 
 answer_routes = Blueprint('answers', __name__)
 
-#get all answers for specific question
+#get all answers
+@answer_routes.route('/')
+@login_required
+def answers():
+    answers = Answer.query.all()
+    return {'answers': [answer.to_dict() for answer in answers]}
 
 
 #get single answer based on id
+@answer_routes.route('/<int:id>/')
+@login_required
+def answer(id):
+    answer = Answer.query.get(id)
+    username = answer.user.username
 
+    answer_info = answer.to_dict()
+    answer_info['username'] = username
+
+    return {'answers': [answer_info]}
 
 #post answer on question
-
+@answer_routes.route('/', methods=['POST'])
+@login_required
+def post_answer():
+    
 
 #edit answer
 
---
+++ app/forms/answer_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError, Length
+from app.models import Answer
+
+
+class AnswerForm(FlaskForm):
+    answer = StringField('answer', validators=[DataRequired, Length(min=2, max=2000, message="Answers must be between 2 and 2000 characters.")])
