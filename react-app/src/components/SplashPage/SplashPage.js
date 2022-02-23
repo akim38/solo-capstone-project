71a112ca8b051ac79e1b7f61857fb2e83f642665
aletheia <aletheiak93@gmail.com>
2022-02-22 23:24:48 -0800

    splash page organized

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import LoginForm from "../auth/LoginForm";
 import SignUpForm from "../auth/SignUpForm";
 
@@ -8,12 +9,21 @@ const SplashPage = () => {
         <div className="background" id="login-background">
             <div className="login-area">
                 <div className="form-box">
-                    <div>
+                    <div className="title-area">
                         <h2 className="title">QueWhat</h2>
                         <p className="intro">A place to ask questions and get answers!</p>
                     </div>
-                    <SignUpForm />
-                    <LoginForm />
+                    <div className="auth-area">
+                        <SignUpForm />
+                        <LoginForm />
+                    </div>
+                    <div className="link-area">
+                        <NavLink to='/about' exact={true} activeClassName='active'>
+                            About
+                        </NavLink>
+                        <a href='https://github.com/akim38' className="about__link" target="_blank" rel="noreferrer">Github</a>
+                        <a href='https://www.linkedin.com/in/aletheia-kim-47086922a/' className="about__link" target="_blank" rel="noreferrer">LinkedIn</a>
+                    </div>
                 </div>
             </div>
         </div>
