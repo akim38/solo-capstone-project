dc8d5f510dd1d3460af5215360a662258a52fda5
aletheia <aletheiak93@gmail.com>
2022-02-22 16:36:15 -0800

    positioned things in splash page

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,12 +1,16 @@
-import LoginForm from "../auth/LoginForm"
-import SignUpForm from "../auth/SignUpForm"
+import LoginForm from "../auth/LoginForm";
+import SignUpForm from "../auth/SignUpForm";
+
+import './SplashPage.css';
 
 const SplashPage = () => {
     return (
-        <div className="background">
-            <div className="form-box">
-                <SignUpForm />
-                <LoginForm />
+        <div className="background" id="login-background">
+            <div className="login-area">
+                <div className="form-box">
+                    <SignUpForm />
+                    <LoginForm />
+                </div>
             </div>
         </div>
     )
