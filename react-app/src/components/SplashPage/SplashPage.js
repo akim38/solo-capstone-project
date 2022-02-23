549a8659a70c8dafaa4b9e6484e99bb561131b69
aletheia <aletheiak93@gmail.com>
2022-02-22 22:44:38 -0800

    adjustment to splash

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -8,6 +8,10 @@ const SplashPage = () => {
         <div className="background" id="login-background">
             <div className="login-area">
                 <div className="form-box">
+                    <div>
+                        <h2 className="title">QueWhat</h2>
+                        <p className="intro">A place to ask questions and get answers!</p>
+                    </div>
                     <SignUpForm />
                     <LoginForm />
                 </div>
