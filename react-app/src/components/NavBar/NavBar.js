9b5d27042635805d55e53d051b87c376992e2157
aletheia <aletheiak93@gmail.com>
2022-02-28 01:03:32 -0800

    centered nav bar and made question detail centered the same place

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -9,31 +9,38 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-          <ion-icon name="home-outline"></ion-icon>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/about' exact={true} activeClassName='active'>
-            About
-          </NavLink>
-        </li>
-        <li>
-          <a href='https://github.com/akim38/solo-capstone-project' className="about__link" target="_blank" rel="noreferrer">
-          <ion-icon name="logo-github"></ion-icon>
-          </a>
-        </li>
-        <li>
-          <a href='https://www.linkedin.com/in/aletheia-kim-47086922a/' className="about__link" target="_blank" rel="noreferrer">
-          <ion-icon name="logo-linkedin"></ion-icon>
-          </a>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      <div className='nav-div'>
+        <ul>
+          {/* <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+            QueWhat
+            </NavLink>
+          </li> */}
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+            <ion-icon name="home-outline"></ion-icon>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/about' exact={true} activeClassName='active'>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <a href='https://github.com/akim38/solo-capstone-project' className="about__link" target="_blank" rel="noreferrer">
+            <ion-icon className='icon' name="logo-github"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/aletheia-kim-47086922a/' className="about__link" target="_blank" rel="noreferrer">
+            <ion-icon className='icon' name="logo-linkedin"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
