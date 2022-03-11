938106143c2d5fb0f0104f3ab897c09d0f457c0c
aletheia <aletheiak93@gmail.com>
2022-03-10 23:12:33 -0800

    created thunks and action creators for comments store

---
+++ react-app/src/store/comments.js
@@ -0,0 +1,121 @@
+//action types
+const LOAD_COMMENTS = 'comment/LOAD';
+const ADD_COMMENT = 'comment/ADD';
+const DELETE_COMMENT = 'comment/DELETE';
+
+
+//action creators
+const loadComments = (comments) => ({
+    type: LOAD_COMMENTS,
+    comments
+});
+
+const addComment = (comment) => ({
+    type: ADD_COMMENT,
+    comment
+});
+
+const deleteComment = (commentId) => ({
+    type: DELETE_COMMENT,
+    commentId
+})
+
+
+//thunks
+export const getCommentsToAnswer = (answerId) => async dispatch => {
+    const res = await fetch(`/api/answers/${answerId}/comments/`)
+
+    if (res.ok) {
+        const comments = await res.json();
+
+        dispatch(loadComments(comments))
+    }
+};
+
+export const getSingleComment = (commentId) => async dispatch => {
+    const res = await fetch(`/api/comments/${commentId}/`);
+
+    if (res.ok) {
+        const comment = await res.json();
+
+        dispatch(loadAnswers(comment))
+    }
+};
+
+export const createComment = (payload, answerId) => async dispatch => {
+    const res = await fetch(`/api/answers/${answerId}/comments/`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+
+    if (res.ok) {
+        const newComment = await res.json();
+        dispatch(addComment(newComment));
+
+        return newComment;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return  {'errors': data.errors}
+        }
+    } else {
+        return ['An error occured. Please try again.']
+    }
+};
+
+export const editComment = (payload, commentId) => async dispatch => {
+    const res = await fetch(`/api/comments/${commentId}/`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+
+    if (res.ok) {
+        const edittedComment = await res.json();
+        dispatch(addComment(edittedComment));
+
+        return edittedComment;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return  {'errors': data.errors}
+        }
+    } else {
+        return ['An error occured. Please try again.']
+    }
+};
+
+export const removeComment = (commentId) => async dispatch => {
+    const res = await fetch(`/api/comments/${commentId}/`, {
+        method: "DELETE"
+    });
+
+    if (res.ok) {
+        const comment = await res.json();
+        dispatch(deleteComment(commentId));
+        return comment;
+    }
+};
+
+//reducer
+const initialState = { byId: {} }
+
+const commentReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case LOAD_COMMENTS: {
+
+        }
+        case ADD_COMMENT: {
+
+        }
+        case DELETE_COMMENT: {
+
+        }
+        default:
+            return state;
+    }
+}
+
+export default commentReducer; 
