e7daa976bb2cdc2b53172d13c20731a9ebbc1234
aletheia <aletheiak93@gmail.com>
2022-03-16 18:37:58 -0700

    removed unused imports and functions in comments component

--- react-app/src/components/Comments/Comments.js
+++ react-app/src/components/Comments/Comments.js
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
 import CommentForm from "../CommentForm/CommentForm";
-import DeleteComment from "./DeleteComment";
-import EditComment from "./EditComment";
 import Comment from "./Comment";
 
 const Comments = ({ answerId, answerComments }) =>  {
-    const sessionUser = useSelector(state => state.session.user);
-    const [showCommentEdit, setShowCommentEdit] = useState(false);
-
-
-    const editComment = () => {
-        setShowCommentEdit(!showCommentEdit)
-    }
-
 
     return (
         <div className="comment-section">
