c4590b58c80c06b2541207d83895b2f1a7808bf7
aletheia <aletheiak93@gmail.com>
2022-03-22 12:03:56 -0700

    created seed for votes

--- app/seeds/answers.py
+++ app/seeds/answers.py
@@ -3,9 +3,9 @@ from app.models import db, Answer
 
 def seed_answers():
 
-    answer_01 = Answer(answer='CHLOROPHYLL!!!!', user_id=2, question_id=1)
-    answer_02 = Answer(answer='I believe it is because plants contain a pigment called chlorophyll which helps the plant absorb sun. The chlorophyll absorbs light but reflects green light so it appears green.', user_id=3, question_id=1)
-    answer_03 = Answer(answer='GIVE TREATS!!!', user_id=2, question_id=2)
+    answer_01 = Answer(answer='CHLOROPHYLL!!!!', user_id=2, question_id=1, upvote_count=0, downvote_count=1)
+    answer_02 = Answer(answer='I believe it is because plants contain a pigment called chlorophyll which helps the plant absorb sun. The chlorophyll absorbs light but reflects green light so it appears green.', user_id=3, question_id=1, upvote_count=1, downvote_count=0)
+    answer_03 = Answer(answer='GIVE TREATS!!!', user_id=2, question_id=2, upvote_count=1, downvote_count=0)
     answer_04 = Answer(answer='Playing with your cat, grooming your cat, giving your cat treats are all great ways to bond with your cat!', user_id=3, question_id=2)
     answer_05 = Answer(answer='Who knows...', user_id=1, question_id=5)
     answer_06 = Answer(answer='Hey, your question could use a little more context.. There are many ways to intepret that question and I am not sure which way you are asking...', user_id=3, question_id=5)
@@ -20,7 +20,7 @@ def seed_answers():
     db.session.add(answer_06)
     db.session.add(answer_07)
     db.session.add(answer_08)
-    
+
     db.session.commit()
 
 
