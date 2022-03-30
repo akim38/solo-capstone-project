79cf066449ca90dfb12cc0ec939c192ced94bd8a
aletheia <aletheiak93@gmail.com>
2022-03-30 13:32:36 -0700

    styled comment section a little bit

--- react-app/src/components/Comments/Comments.js
+++ react-app/src/components/Comments/Comments.js
@@ -10,7 +10,7 @@ const Comments = ({ answerId, answerComments }) =>  {
             <CommentForm  answerId={answerId} />
             {answerComments.map(comment => (
                 <div className="comment-box" key={comment.comment}>
-                    <h5><ion-icon size="large" name="person-circle-outline"></ion-icon> {comment.username}</h5>
+                    <p><ion-icon size="large" name="person-circle-outline"></ion-icon> {comment.username}</p>
                     <Comment comment={comment} />
                 </div>
             ))}
