e6837a3972f286af38094524a9a4c71a382c547e
aletheia <aletheiak93@gmail.com>
2022-03-30 20:46:37 -0700

    cleaned up some unused imports in answers component

--- react-app/src/components/Answers/Answers.js
+++ react-app/src/components/Answers/Answers.js
@@ -1,14 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getQuestionAnswers } from "../../store/answers";
-import DeleteAnswer from "./DeleteAnswer";
 
 import './Answers.css'
-import AnswerEditFormModal from "../AnswerEditForm/AnswerEditFormModal";
 import Comments from "../Comments/Comments";
-import Votes from "../Votes/Votes";
-import CommentForm from "../CommentForm/CommentForm";
 import Answer from "./Answer";
 
 
@@ -18,8 +14,6 @@ const Answers = () => {
     const dispatch = useDispatch();
     const answers = useSelector(state => state.answers.byId)
     const answerList = Object.values(answers)
-    const sessionUser = useSelector(state => state.session.user)
-    const [showButtons, setShowButtons] = useState(false);
 
     useEffect(() => {
         dispatch(getQuestionAnswers(questionId))
